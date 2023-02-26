88f075eafff2d204f15003f3b06b8f079482f9cb
heysebas <sebazgrisales@gmail.com>
2023-02-25 19:25:35 -0500

    tabla alunos

--- src/main/java/com/colegio/colegio/controller/IndexController.java
+++ src/main/java/com/colegio/colegio/controller/IndexController.java
@@ -1,5 +1,32 @@
 package com.colegio.colegio.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.colegio.colegio.models.entity.Profesor;
+
+@Controller
+/*@RequestMapping("/index")*/
 public class IndexController {
-    
+    @GetMapping({"/","","/index"})
+    public String indeString(Model model){
+        Profesor profesor = new Profesor();
+        model.addAttribute("titulo", "Perfil");
+        model.addAttribute("profesor",profesor);
+        profesor.setNombre("Alvaro Antonio");
+        profesor.setApellido("Rodriguez Fernandez");
+        profesor.setCc("123456789");
+        profesor.setTelefono("3508773820");
+        profesor.setEmail("AlvaroAntonio@gmail.com");
+        profesor.setDireccion("CALLE 23 14");
+        profesor.setEdad(40);
+        model.addAttribute("cuerpo", "perfil profesor");
+        return "/index";
+        
+    }
 }
+
+
+
--- src/main/java/com/colegio/colegio/models/entity/alumnos.java
+++ src/main/java/com/colegio/colegio/models/entity/alumnos.java
@@ -10,12 +10,15 @@ public class alumnos {
     private String direccion;
     private int edad;
     private String grado;
+    private double nota;
+    
 
     public alumnos() {
     }
 
+
     public alumnos(String nombre, String apellido, String cc, String telefono, String email, String direccion, int edad,
-            String grado) {
+            String grado, double nota) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.cc = cc;
@@ -24,70 +27,99 @@ public class alumnos {
         this.direccion = direccion;
         this.edad = edad;
         this.grado = grado;
+        this.nota = nota;
     }
+
 
     public String getNombre() {
         return nombre;
     }
 
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
 
     public String getApellido() {
         return apellido;
     }
 
+
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
+
 
     public String getCc() {
         return cc;
     }
 
+
     public void setCc(String cc) {
         this.cc = cc;
     }
+
 
     public String getTelefono() {
         return telefono;
     }
 
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
+
 
     public String getEmail() {
         return email;
     }
 
+
     public void setEmail(String email) {
         this.email = email;
     }
+
 
     public String getDireccion() {
         return direccion;
     }
 
+
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
+
 
     public int getEdad() {
         return edad;
     }
 
+
     public void setEdad(int edad) {
         this.edad = edad;
     }
+
 
     public String getGrado() {
         return grado;
     }
 
+
     public void setGrado(String grado) {
         this.grado = grado;
     }
+
+
+    public double getNota() {
+        return nota;
+    }
+
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    
 
 }
